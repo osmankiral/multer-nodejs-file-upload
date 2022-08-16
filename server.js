c02280982fa6fc47821661a870c731b5a8946a7e
osmankiral96 <96803197+osmankiral96@users.noreply.github.com>
2022-08-16 21:31:27 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const multer = require('multer')
+const uploadMiddleware = multer({
+    limits:{
+        fileSize: 1024 * 1024 * 20
+    },
+    fileFilter: (req, file, cb) => {
+        cb(undefined, true)
+    },
+    storage: multer.diskStorage({
+        filename: (req, file, cb) =>{
+            cb(null, file.originalname)
+        },
+        destination: (req, file, cb) => {
+            cb(null, 'uploads/')
+        }
+    })
+})
+
+// GET / - Yükleme formunun bulunduğu html dosyası
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+// POST /upload - Yükleme formundan gelen dosyaları local klasöre ekleyen URL
+app.post('/upload', uploadMiddleware.single('avatar'),(req, res) => {
+    if(!req.file){
+        return res.json({
+            success: false,
+            message: "Dosya Yüklenemedi"
+        })
+    }
+    // return success
+    return res.json({
+        succes: true,
+        message: "Dosya Yüklendi. Tebrikler...",
+        file: req.file
+    })
+})
+
+// GET /uploads/:filename - Yükleme formundan gelen dosyaları local klasöre ekleyen URL
+app.get('/uploads/:filename', (req, res) => {
+    var filename = req.params.filename
+    res.sendFile(__dirname + '/uploads/' + filename)
+})
+
+
+app.listen(9090, () => {
+    console.log('server is running on port http:/localhost:9090')
+})
